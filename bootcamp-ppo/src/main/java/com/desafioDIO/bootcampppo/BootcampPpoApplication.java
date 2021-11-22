544c5fe2990561abdac8f7d2aeedfb768e91294d
Fellipe <fellipetoledo.dev@gmail.com>
2021-11-22 11:00:24 -0300

    Adicionado projeto bootcamp/ppo

---
+++ bootcamp-ppo/src/main/java/com/desafioDIO/bootcampppo/BootcampPpoApplication.java
@@ -0,0 +1,13 @@
+package com.desafioDIO.bootcampppo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootcampPpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootcampPpoApplication.class, args);
+	}
+
+}
---
+++ bootcamp-ppo/src/test/java/com/desafioDIO/bootcampppo/BootcampPpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioDIO.bootcampppo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootcampPpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
